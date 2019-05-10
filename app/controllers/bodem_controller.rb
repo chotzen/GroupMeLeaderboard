4877b44212b9766b6f30975936560a5225367579
Devin Hartzell <dchotzen-hartzell19@punahou.edu>
2019-05-09 19:27:48 -1000

    fix

--- app/controllers/bodem_controller.rb
+++ app/controllers/bodem_controller.rb
@@ -3,7 +3,7 @@ class BodemController < ApplicationController
   protect_from_forgery with: :null_session
   def scanner
     if params[:text] =~ /!ping/
-      HTTParty.post('https://api.groupme.com/v3/bots/post', body: { text: "Hmmm", 
+      HTTParty.post('https://api.groupme.com/v3/bots/post', body: { text: "Pong", 
                                                                     bot_id: "ccf65eaeb8ad96b1c3f1a08928"})
 
     end
